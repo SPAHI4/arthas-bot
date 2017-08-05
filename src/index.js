d752145a71375ff5e773a596906e96db9307bdae
Anton S <spaxi4@yandex.ru>
2017-08-05 04:10:08 +0300

    Added production build

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import commandInline from './inline'
 
 const app = new Telegraf(process.env.BOT_TOKEN);
 
+console.log('Я вернулся из небытия... ДА ДА Я');
 
 app.on('inline_query', commandInline);
 
--- src/inline/index.js
+++ src/inline/index.js
@@ -10,14 +10,13 @@ export default async (ctx) => {
                 type: 'voice',
                 title: text,
                 voice_url: sounds[text],
-                performer: 'Папич',
                 id: idx.toString(),
             }))
             .filter(item => item.title.toUpperCase().includes(ctx.update.inline_query.query.toUpperCase()))
-            .slice(ctx.update.inline_query.offset || 0, CHUNK_COUNT)
+            .slice(Number(ctx.update.inline_query.offset || 0), CHUNK_COUNT)
         ,
         {
-            next_offset: ctx.update.inline_query.offset + CHUNK_COUNT,
+            next_offset: (Number(ctx.update.inline_query.offset) + CHUNK_COUNT).toString(),
         },
     );
 }
