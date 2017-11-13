1a3931fa84c9ec30c47912f9eaf481211e11f290
Anton S <spaxi4@yandex.ru>
2017-11-13 03:25:59 +0300

    Update soundsList.js

--- src/sounds/soundsList.js
+++ src/sounds/soundsList.js
@@ -1,5 +1,7 @@
 import sounds from './sounds'
 
 export default (ctx) => {
-    ctx.reply(Object.keys(sounds).join('\r\n'));
+    ctx.reply(`Список всех фраз для инлайн режима: @HailrakeBot
+
+${Object.keys(sounds).join('\r\n')}`);
 }
