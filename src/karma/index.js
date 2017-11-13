50e809fe9f75cfbc8ba5f8c8ec7fa2ad8b01f912
Anton S <spaxi4@yandex.ru>
2017-11-14 02:00:59 +0300

    Update index.js

--- src/karma/index.js
+++ src/karma/index.js
@@ -2,8 +2,8 @@ import { sample, random } from 'lodash';
 import { User } from '../db/entity/User';
 import { esc, getUsername } from '../utils';
 
-export const PLUS_TRIGGERS = ['\+', 'СПС', 'ДЯКУЮ', 'ОРУ', 'ПЛЮС', '👍', 'ТУПА ЛИКЕ'];
-export const MINUS_TRIGGERS = ['\-', 'МИНУС', 'ДАУН', '👎'];
+export const PLUS_TRIGGERS = ['СПС', 'ДЯКУЮ', 'ОРУ', 'ПЛЮС', '👍', 'ТУПА ЛИКЕ'];
+export const MINUS_TRIGGERS = ['МИНУС', 'ДАУН', '👎'];
 
 
 export const karmaPlus = async (ctx) => {
