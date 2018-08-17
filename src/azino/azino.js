a910c3a942b27be1d14beb358881ad19b5cca3f9
Anton S <spaxi4@yandex.ru>
2018-08-17 22:56:41 +0200

    fix cd

--- src/azino/azino.js
+++ src/azino/azino.js
@@ -8,7 +8,7 @@ import { User } from '../db/entity/User';
 import { esc, limiter, replyOnly, withUser } from '../utils';
 
 const IS_PROD = process.env.NODE_ENV === 'production';
-export const CASINO_COOLDOWN = 10;
+export const CASINO_COOLDOWN = 20;
 const REQUIRED_KARMA = 1;
 const DEFAULT_BET = 5;
 
