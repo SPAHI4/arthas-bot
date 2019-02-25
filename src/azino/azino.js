5f8a9b7c157604ef327ac6ad88c92c5ba27a93f5
Anton S <spaxi4@yandex.ru>
2019-02-25 17:42:06 +0300

    increased casino cd

--- src/azino/azino.js
+++ src/azino/azino.js
@@ -8,7 +8,7 @@ import { User } from '../db/entity/User';
 import { esc, limiter, replyOnly, withUser } from '../utils';
 
 const IS_PROD = process.env.NODE_ENV === 'production';
-export const CASINO_COOLDOWN = 60;
+export const CASINO_COOLDOWN = 90;
 const REQUIRED_KARMA = 1;
 const DEFAULT_BET = 5;
 
