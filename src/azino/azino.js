f111288fca8742756614277769731e387132d2d6
Anton Izmailov <wapgear4@gmail.com>
2019-04-11 13:13:10 +0300

    Update azino.js

--- src/azino/azino.js
+++ src/azino/azino.js
@@ -100,7 +100,7 @@ const casinoImpl = async ({ message, reply, replyWithHTML, replyWithHTMLQuote, u
 
 		if (IS_PROD && user.lastCasino && differenceInMinutes(new Date(), user.lastCasino) < CASINO_COOLDOWN) {
 			isBusy = false;
-			const waitingTime = `ПРИХОДИ ЧЕРЕЗ ${differenceInMinutes(new Date(), user.lastCasino)} МИНУТ`;
+			const waitingTime = `ПРИХОДИ ЧЕРЕЗ ${differenceInMinutes(new Date(), user.lastCasino)} ${pluralize(all, 'МИНУТУ', 'МИНУТЫ', 'МИНУТ')}`;
 			return replyWithHTMLQuote(sample([
 				`АВТИКИ ПОКА ЗАКРЫТЫ ДЛЯ ТЕБЯ, ${waitingTime}`,
 				`НОТ ЭНАФ МАНА, ${waitingTime}`,
